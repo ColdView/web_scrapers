0a2aa543a2e810ca95a57d9461818054c141638f
ColdView <vinnimacc@mail.com>
2017-08-10 20:37:25 +0100

    Fixed issues after site update

--- hot_uk_scraper.py
+++ hot_uk_scraper.py
@@ -7,44 +7,56 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-query = str(input("Please enter product name "))
-parameters = {"action": "search", "keywords": query}
+query = "ps4"#str(input("Please enter product name "))
+parameters = {"q": query}
 page = requests.get("http://www.hotukdeals.com/search?", params=parameters)
 soup = BeautifulSoup(page.content, "html.parser")
-container = soup.find_all(class_="space--mt-3")[5]
+source = soup.find(class_="thread-list--type-list")
 
 titles = []
 retailers = []
+price_data= []
 prices = []
 temps = []
 links = []
 URL = "http://www.hotukdeals.com"
 
-
 # Pull titles, clean and append to titles list
-title_list = [t.get_text() for t in container.select("a.thread-title-text")]
-[titles.append(t.split("£")[0].strip(' -')) for t in title_list]
+[titles.append(t.get_text().split("£")[0].strip()) for t in source.select("a.cept-tt")]
+# Pull retailer and append to retailer list
+[retailers.append(r.get_text()) for r in source.select(".thread-group")]
 
-# Pull price, retailer, clean and append to price and retailer lists
-data = [p.get_text() for p in container.select(".vwo-title-info")]
-[retailers.append(r.split("@ ")[1].split("\n")[0]) for r in data]
-[prices.append(p.split(" @")[0]) for p in data]
+[price_data.append(i.get_text()) for i in source.select(".thread-listImgCell")]
 
-# Pull temps, convert to int's and append to temps list
-[temps.append(int(t.get_text())) for t in container.select(".vwo-vote-container .vwo-temperature")]
+for p in price_data:
+	if "£" in p:
+		x = p.strip("GetGet deal £")
+		prices.append(float(x))
+	else:
+		prices.append(0.00)
+# Pull temps, convert to int's remove trailing degree symbol and append to temps list
 
-# Pull paths to details page, append to url then append to links list
-paths = [p["href"] for p in container.select("a.thread-title-text")]
-[links.append(URL + p) for p in paths]
+for t in source.select(".vote-temp"):
+	t = t.get_text().rstrip("°")
+	try:	
+		temps.append(int(t))
+	except:
+		temps.append(0)
+
+[links.append(p["href"]) for p in source.select("a.cept-tt.thread-link")]
 
 # Create Pandas table and sort on temp
-table = pd.DataFrame({
-        "link": links, 
-        "retailer": retailers, 
-        "temp": temps, 
+
+df = pd.DataFrame({
+        "title": titles,
+        "temp": temps,
         "price": prices,
-        "title": titles, 
+        "retailer": retailers,
+        "link": links
     })
-temp_sort = table.sort_values("temp", ascending=False)
-print(temp_sort)
-#test
+table = df[["title", "temp", "price", "retailer", "link"]]
+pd.set_option('display.max_colwidth', 250)
+pd.set_option('max_rows', 100)
+pd.set_option('colheader_justify', 'left')
+table.sort_values("temp", inplace=True, ascending=False)
+table.to_html('deals_table.html', escape=False)
